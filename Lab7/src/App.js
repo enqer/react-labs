2316ae9a863a2db85938e06f8fc021662bc78d09
Paweł <pawel.kam4@gmail.com>
2023-04-30 11:03:54 +0200

    lab7

---
+++ Lab7/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Test1 from "./components/componentClass";
+import Test2 from "./components/componentFunctional";
+import QuizComponent from "./components/QuizComponent";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+      <QuizComponent/>
+
+  );
+}
+
+export default App;
