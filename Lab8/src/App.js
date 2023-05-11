999b2bd873d71cc164e8700195f06f1fe8af3dc0
Paweł <pawel.kam4@gmail.com>
2023-05-11 23:14:31 +0200

    lab8

---
+++ Lab8/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Posts from "./components/posts";
+
+function App() {
+  return (
+    <main className="container">
+      <Posts/>
+    </main>
+  );
+}
+
+export default App;
